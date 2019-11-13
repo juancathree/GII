d9d47417e61cff111f4a6258cb64d4a5f5fa32dc
juan <juancarloslucenamonje@gmail.com>
2019-11-13 22:20:43 +0100

    cellValues

--- DA/Practicas/LUCENA_MONJE_JUANCARLOS/p1/DefenseStrategy.cpp
+++ DA/Practicas/LUCENA_MONJE_JUANCARLOS/p1/DefenseStrategy.cpp
@@ -21,42 +21,46 @@ using namespace Asedio;
 
 Vector3 cellCenterToPosition(int i, int j, float cellWidth, float cellHeight){ return Vector3((j * cellWidth) + cellWidth * 0.5f, (i * cellHeight) + cellHeight * 0.5f, 0); }
 
-float cellValue(int row, int col, int nCellsWidth, int nCellsHeight, float mapWidth, float mapHeight, List<Object*> obstacles, Defense* base) {
+float cellValue(int row, int col, int nCellsWidth, int nCellsHeight, float mapWidth, float mapHeight, List<Object*> obstacles, Defense* base, bool first) {
 	int value = 0;
     float distance;
     float cellWidth = mapWidth / nCellsWidth;
     float cellHeight = mapHeight / nCellsHeight;
     List<Object*>::iterator currentObstacle = obstacles.begin();
-    bool choca = false;
 
-    Vector3 positionTmp = cellCenterToPosition(row, col, cellWidth, cellHeight);
-    base->position.x = positionTmp.x;
-    base->position.y = positionTmp.y;
 
-    while(currentObstacle != obstacles.end() && !choca){
-        distance = _distance(base->position, (*currentObstacle)->position) - (base->radio + (*currentObstacle)->radio);
-        if((distance < 2*cellWidth || distance < 2*cellHeight) && distance > 0 ){
-            value++;
+    if(first){
+        bool choca = false;
+        Vector3 positionTmp = cellCenterToPosition(row, col, cellWidth, cellHeight);
+        base->position.x = positionTmp.x;
+        base->position.y = positionTmp.y;
+
+        while(currentObstacle != obstacles.end() && !choca){
+            distance = _distance(base->position, (*currentObstacle)->position) - (base->radio + (*currentObstacle)->radio);
+            if((distance < 2*cellWidth || distance < 2*cellHeight) && distance > 0 ){
+                value++;
+            }
+            else if(distance < 0){
+                value = 0;
+                choca = true;
+            }
+            currentObstacle++;
         }
-        else if(distance < 0){
-            value = 0;
-            choca = true;
-        }
-        currentObstacle++;
     }
-    // else{
-    //     Vector3 defense = cellCenterToPosition(row, col, cellWidth, cellHeight);
-    //     distance = _distance(defense, base->position) ;
-    //     bool tieneValor = false;
-    //     int i=1;
-    //     while(!tieneValor){
-    //         if(distance < i*cellWidth || distance < i*cellHeight){
-    //             value = 100-i;
-    //             tieneValor = true;
-    //         }
-    //         i++;
-    //     }
-    // }
+
+    else{
+        Vector3 defense = cellCenterToPosition(row, col, cellWidth, cellHeight);
+        distance = _distance(defense, base->position) ;
+        bool tieneValor = false;
+        int i=1;
+        while(!tieneValor){
+            if(distance < i*cellWidth || distance < i*cellHeight){
+                value = 100-i;
+                tieneValor = true;
+            }
+            i++;
+        }
+    }
 	return value; // implemente aqui la funci�n que asigna valores a las celdas
 }
 
@@ -93,8 +97,37 @@ void DEF_LIB_EXPORTED placeDefenses(bool** freeCells, int nCellsWidth, int nCell
     std::list<Defense*> defensesPlaced;
     int maxAttemps = 1000;
     List<Defense*>::iterator currentDefense = defenses.begin();
-    int value = cellValue(29, 50, nCellsWidth, nCellsHeight, mapWidth, mapHeight, obstacles, (*currentDefense));
-    std::cout <<  value << std::endl;
+    
+    int max = 0, baseRow, baseCol;
+    float** cellValues = new float*[nCellsHeight];
+    for(int i=0; i<nCellsHeight; i++){
+        cellValues[i] = new float[nCellsWidth];
+        for(int j=0; j<nCellsWidth; j++){
+            cellValues[i][j] = cellValue(i,j, nCellsWidth, nCellsHeight, mapWidth, mapHeight, obstacles, (*currentDefense), true);
+            if(max < cellValues[i][j]){    
+                baseRow = i; baseCol = j;
+                max = cellValues[i][j];
+            }
+        }
+    }
+
+    std::cout << baseRow << std::endl;
+    std::cout << baseCol << std::endl;
+
+    Vector3 basePosition = cellCenterToPosition(baseRow, baseCol, cellWidth, cellHeight);
+    (*currentDefense)->position.x = basePosition.x;
+    (*currentDefense)->position.y = basePosition.y;
+
+
+     for(int i=0; i<nCellsHeight; i++){
+        cellValues[i] = new float[nCellsWidth];
+        for(int j=0; j<nCellsWidth; j++){
+            cellValues[i][j] = cellValue(i,j, nCellsWidth, nCellsHeight, mapWidth, mapHeight, obstacles, (*currentDefense), false);
+        }
+    }
+
+    std::cout << cellValues[6][25] << std::endl;
+
     while(currentDefense != defenses.end() && maxAttemps > 0) {
 
         do{
