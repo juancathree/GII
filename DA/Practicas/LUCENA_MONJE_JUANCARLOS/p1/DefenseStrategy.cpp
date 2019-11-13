89eeaca60cb6ecf58d27ed3732e4454fccf41866
juan <juancarloslucenamonje@gmail.com>
2019-11-13 21:44:02 +0100

    cellValue

--- DA/Practicas/LUCENA_MONJE_JUANCARLOS/p1/DefenseStrategy.cpp
+++ DA/Practicas/LUCENA_MONJE_JUANCARLOS/p1/DefenseStrategy.cpp
@@ -19,9 +19,45 @@ RAND_TYPE SimpleRandomGenerator::a;
 
 using namespace Asedio;
 
-float cellValue(int row, int col, bool** freeCells, int nCellsWidth, int nCellsHeight
-	, float mapWidth, float mapHeight, List<Object*> obstacles, List<Defense*> defenses) {
-	return 0; // implemente aqui la funci�n que asigna valores a las celdas
+Vector3 cellCenterToPosition(int i, int j, float cellWidth, float cellHeight){ return Vector3((j * cellWidth) + cellWidth * 0.5f, (i * cellHeight) + cellHeight * 0.5f, 0); }
+
+float cellValue(int row, int col, int nCellsWidth, int nCellsHeight, float mapWidth, float mapHeight, List<Object*> obstacles, Defense* base) {
+	int value = 0;
+    float distance;
+    float cellWidth = mapWidth / nCellsWidth;
+    float cellHeight = mapHeight / nCellsHeight;
+    List<Object*>::iterator currentObstacle = obstacles.begin();
+    bool choca = false;
+
+    Vector3 positionTmp = cellCenterToPosition(row, col, cellWidth, cellHeight);
+    base->position.x = positionTmp.x;
+    base->position.y = positionTmp.y;
+
+    while(currentObstacle != obstacles.end() && !choca){
+        distance = _distance(base->position, (*currentObstacle)->position) - (base->radio + (*currentObstacle)->radio);
+        if((distance < 2*cellWidth || distance < 2*cellHeight) && distance > 0 ){
+            value++;
+        }
+        else if(distance < 0){
+            value = 0;
+            choca = true;
+        }
+        currentObstacle++;
+    }
+    // else{
+    //     Vector3 defense = cellCenterToPosition(row, col, cellWidth, cellHeight);
+    //     distance = _distance(defense, base->position) ;
+    //     bool tieneValor = false;
+    //     int i=1;
+    //     while(!tieneValor){
+    //         if(distance < i*cellWidth || distance < i*cellHeight){
+    //             value = 100-i;
+    //             tieneValor = true;
+    //         }
+    //         i++;
+    //     }
+    // }
+	return value; // implemente aqui la funci�n que asigna valores a las celdas
 }
 
 bool factibilidad(Defense* defense, std::list<Object*> obstacles, std::list<Defense*> defensesPlaced, float mapWidth, float mapHeight){
@@ -57,11 +93,13 @@ void DEF_LIB_EXPORTED placeDefenses(bool** freeCells, int nCellsWidth, int nCell
     std::list<Defense*> defensesPlaced;
     int maxAttemps = 1000;
     List<Defense*>::iterator currentDefense = defenses.begin();
+    int value = cellValue(29, 50, nCellsWidth, nCellsHeight, mapWidth, mapHeight, obstacles, (*currentDefense));
+    std::cout <<  value << std::endl;
     while(currentDefense != defenses.end() && maxAttemps > 0) {
 
         do{
-            (*currentDefense)->position.x = ((int)(_RAND2(10))) * cellWidth + cellWidth * 0.5f;
-            (*currentDefense)->position.y = ((int)(_RAND2(10))) * cellHeight + cellHeight * 0.5f;
+            (*currentDefense)->position.x = ((int)(_RAND2(nCellsWidth))) * cellWidth + cellWidth * 0.5f;
+            (*currentDefense)->position.y = ((int)(_RAND2(nCellsHeight))) * cellHeight + cellHeight * 0.5f;
             (*currentDefense)->position.z = 0; 
         }while(!factibilidad((*currentDefense), obstacles, defensesPlaced, mapWidth, mapHeight));
         
