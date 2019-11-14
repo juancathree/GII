165d7e8fe549bf3ded1e1d152932649792831009
juan <juancarloslucenamonje@gmail.com>
2019-11-14 18:09:58 +0100

    funciona

--- DA/Practicas/LUCENA_MONJE_JUANCARLOS/p1/DefenseStrategy.cpp
+++ DA/Practicas/LUCENA_MONJE_JUANCARLOS/p1/DefenseStrategy.cpp
@@ -20,58 +20,76 @@ RAND_TYPE SimpleRandomGenerator::a;
 using namespace Asedio;
 
 struct Cells{
-    int row, col, value;
-    int sort(){return 18;}
+    int row=0, col=0;
+    float value=0;
 };
 
-List<Cells> ordenar(List<Cells> celdas){
+void ordenar(List<Cells> &celdas){
     List<Cells>::iterator currentCell;
-    List<Cells>::iterator iteratorAux;
-    List<Cells> cells;
-    int i;
+    List<Cells>::iterator nextCell;
     Cells aux;
+    int i=0;
     while(i < celdas.size()-1){
-        i=0;
+        i = 0;
         currentCell = celdas.begin();
         while(currentCell != celdas.end()){
-            iteratorAux = currentCell;
-            iteratorAux++;
-            if((*currentCell).value < (*iteratorAux).value){
-                std::iter_swap(currentCell, iteratorAux);
+            nextCell = currentCell;
+            nextCell++;
+            if((*currentCell).value < (*nextCell).value){
+                aux.row = (*currentCell).row;
+                aux.col = (*currentCell).col;
+                aux.value = (*currentCell).value;
+                (*currentCell).row = (*nextCell).row;
+                (*currentCell).col = (*nextCell).col;
+                (*currentCell).value = (*nextCell).value;
+                (*nextCell).row = aux.row;
+                (*nextCell).col = aux.col;
+                (*nextCell).value = aux.value;                             
             }
-            else{
-                currentCell++;
-                i++;
-            }
+            else i++;
+            currentCell++;
         }
     }
-    cells = celdas;
-    return cells;
 }
 
 Vector3 cellCenterToPosition(int i, int j, float cellWidth, float cellHeight){ return Vector3((j * cellWidth) + cellWidth * 0.5f, (i * cellHeight) + cellHeight * 0.5f, 0); }
 
 float cellValue(int row, int col, int nCellsWidth, int nCellsHeight, float mapWidth, float mapHeight, List<Object*> obstacles, Defense* base, bool first) {
-    int value = 0;
+    float value = 0;
     float distance;
     float cellWidth = mapWidth / nCellsWidth;
     float cellHeight = mapHeight / nCellsHeight;
     List<Object*>::iterator currentObstacle = obstacles.begin();
 
     if(first){
-        bool choca = false;
         Vector3 positionTmp = cellCenterToPosition(row, col, cellWidth, cellHeight);
         base->position.x = positionTmp.x;
         base->position.y = positionTmp.y;
-
-        while(currentObstacle != obstacles.end() && !choca){
+        bool posUp = false, posDown = false, posLeft = false, posRight = false;
+        while(currentObstacle != obstacles.end()){
             distance = _distance(base->position, (*currentObstacle)->position) - (base->radio + (*currentObstacle)->radio);
-            if((distance < 2*cellWidth || distance < 2*cellHeight) && distance > 0 ){
+            if(distance > 4 && distance < 10){
                 value++;
-            }
-            else if(distance < 0){
-                value = 0;
-                choca = true;
+                if(base->position.x < (*currentObstacle)->position.x){
+                    if(posUp || posLeft || posRight) value++;
+                    posDown = true;
+                    value++;
+                }
+                if(base->position.x > (*currentObstacle)->position.x){
+                    if(posDown || posLeft || posRight) value++;
+                    posUp = true;
+                    value++;
+                }
+                if(base->position.y > (*currentObstacle)->position.y){
+                    if(posUp || posDown || posRight) value++;
+                    posLeft = true;
+                    value++;
+                }
+                if(base->position.x < (*currentObstacle)->position.x){
+                    if(posUp || posLeft || posDown) value++;
+                    posRight = true;
+                    value++;
+                }
             }
             currentObstacle++;
         }
@@ -81,13 +99,21 @@ float cellValue(int row, int col, int nCellsWidth, int nCellsHeight, float mapWi
         Vector3 defense = cellCenterToPosition(row, col, cellWidth, cellHeight);
         distance = _distance(defense, base->position) ;
         bool tieneValor = false;
-        int i=1;
+        float i=1;
         while(!tieneValor){
             if(distance < i*cellWidth || distance < i*cellHeight){
                 value = 100-i;
                 tieneValor = true;
             }
             i++;
+        }
+        while(currentObstacle != obstacles.end()){
+            Vector3 positionTmp = cellCenterToPosition(row, col, cellWidth, cellHeight);
+            distance = _distance(positionTmp, (*currentObstacle)->position);
+            if(distance > 29 && distance < 33){
+                value++;
+            }
+            currentObstacle++;
         }
     }
 	return value; // implemente aqui la funci�n que asigna valores a las celdas
@@ -123,14 +149,14 @@ void DEF_LIB_EXPORTED placeDefenses(bool** freeCells, int nCellsWidth, int nCell
 
     float cellWidth = mapWidth / nCellsWidth;
     float cellHeight = mapHeight / nCellsHeight; 
-    std::list<Defense*> defensesPlaced;
-    int maxAttemps = 1000;
+    List<Defense*> defensesPlaced;
     List<Defense*>::iterator currentDefense = defenses.begin();
 
+    // Asignacion valores para la base
     List<Cells> cells;
     Cells aux;
-    for(int i=0; i< nCellsHeight; i++){
-        for(int j=0; j< nCellsWidth; j++){ 
+    for(int i=0; i < nCellsHeight; i++){
+        for(int j=0; j < nCellsWidth; j++){ 
             aux.row = i;
             aux.col = j;
             aux.value = cellValue(i,j, nCellsWidth, nCellsHeight, mapWidth, mapHeight, obstacles, (*currentDefense), true); 
@@ -138,26 +164,55 @@ void DEF_LIB_EXPORTED placeDefenses(bool** freeCells, int nCellsWidth, int nCell
         }
     }
 
-    List<Cells> celdasOrdenadas = ordenar(cells);
-    List<Cells>::iterator currentCell = celdasOrdenadas.begin();
-    int q = 12;
-    while(q>0){
-        std::cout << (*currentCell).value << std::endl;
-        q--;
+    // Ordenar la lista para la base
+    ordenar(cells);
+
+    List<Cells>::iterator currentCell = cells.begin();
+    (*currentDefense)->position.x=0;
+    
+    // algoritmo voraz base
+    while((*currentDefense)->position.x ==0 && currentCell != cells.end()){
+        Vector3 currentPosition = cellCenterToPosition((*currentCell).row, (*currentCell).col, cellWidth, cellHeight);
+        (*currentDefense)->position.x = currentPosition.x;
+        (*currentDefense)->position.y = currentPosition.y;
+        if(!factibilidad((*currentDefense), obstacles, defensesPlaced, mapWidth, mapHeight)){
+            (*currentDefense)->position.x = 0;
+        }
         currentCell++;
     }
-
-
-    while(currentDefense != defenses.end() && maxAttemps > 0) {
-
-        do{
-            (*currentDefense)->position.x = ((int)(_RAND2(nCellsWidth))) * cellWidth + cellWidth * 0.5f;
-            (*currentDefense)->position.y = ((int)(_RAND2(nCellsHeight))) * cellHeight + cellHeight * 0.5f;
-            (*currentDefense)->position.z = 0; 
-        }while(!factibilidad((*currentDefense), obstacles, defensesPlaced, mapWidth, mapHeight));
+    defensesPlaced.push_back((*currentDefense));
+    
+    // Asignacion valores para las demas
+    List<Cells> cells2;
+    for(int i=0; i < nCellsHeight; i++){
+        for(int j=0; j < nCellsWidth; j++){ 
+            aux.row = i;
+            aux.col = j;
+            aux.value = cellValue(i,j, nCellsWidth, nCellsHeight, mapWidth, mapHeight, obstacles, (*currentDefense), false); 
+            cells2.push_back(aux);
+        }
+    }
+    currentDefense++;
         
+    // Ordenar la lista para las demas
+    ordenar(cells2);
+
+    List<Cells>::iterator currentCell2 = cells2.begin();
+
+    // algoritmo voraz demas
+    while(currentDefense != defenses.end()){
+        (*currentDefense)->position.x = 0;
+        while((*currentDefense)->position.x == 0 && currentCell2 != cells2.end()){
+            Vector3 currentPosition = cellCenterToPosition((*currentCell2).row, (*currentCell2).col, cellWidth, cellHeight);
+            (*currentDefense)->position.x = currentPosition.x;
+            (*currentDefense)->position.y = currentPosition.y;
+            if(!factibilidad((*currentDefense), obstacles, defensesPlaced, mapWidth, mapHeight)){
+                (*currentDefense)->position.x = 0;
+            }
+            currentCell2++;
+        }
         defensesPlaced.push_back((*currentDefense));
-        ++currentDefense;
+        currentDefense++;
     }
 
 #ifdef PRINT_DEFENSE_STRATEGY
